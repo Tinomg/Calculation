67f96b1c7781c0e3513cc145952b4080a1bdabda
tinom@gmail.com <tinomg@gmail.com>
2019-04-02 18:18:54 +0300

    Allowed characters and error mesage

--- operation.py
+++ operation.py
@@ -1,4 +1,4 @@
-#operation
+# operation
 def mult(first_num,second_num):
     return first_num*second_num
 def dev(first_num,second_num):
@@ -13,16 +13,24 @@ def sub(first_num,second_num):
     return first_num-second_num
 operation={'*':mult , '/':dev , '+':add , '-':sub}
 
-
+# Getting adn checking user input
+#allowed characters
+allowed_chars=('0','1','2','2','4','5','6','7','8','9','*','/','+','-','(',')','.')
+#Uncorect mesage function
+def error_message(uncorrect_char):
+    return (f'Not corect input {uncorrect_char}')
 user_expression=input('Write expresion')
 split_expression=[]
 number=''
 for chart in user_expression:
-    if chart.isdecimal() or chart=='.':
+    if chart in allowed_chars:
+      if chart.isdecimal() or chart=='.':
         number+=chart
-    else:
+      else:
         split_expression.append(number)
         split_expression.append(chart)
         number=''
+    else :
+        print(error_message)
 split_expression.append(number)
 print(split_expression)
