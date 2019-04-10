ef8b19c5f3b64b4e2168ed5825b3c597401b19e9
tinom@gmail.com <tinomg@gmail.com>
2019-04-10 15:04:34 +0300

    4

--- operation.py
+++ operation.py
@@ -24,40 +24,45 @@ def user_input():
 
 #disasemblig brackets and returning star,end and slice for operation
 def split_brackets(expression):
-  if '(' in expression and ')' in expression:
+  
     bracket_patern=r"\([^\(\)]+\)"
-    brack_string=re.sub(bracket_patern,split_expression(expression),expression)
-    return brack_string
-  else:
+    repl_string=(re.search(bracket_patern,expression).group()).strip('(').strip(')')
+    expression=re.sub(bracket_patern,split_expression(repl_string),expression,1)
     return expression
-
 # claculate 
 def calculation(expr_string,operator):
   expr_list=expr_string.split(operator)
-  return operation[operator](float(expr_list[0]),float(expr_list[1]))
+  return str(operation[operator](float(expr_list[0]),float(expr_list[1])))
 
 #split simple expresion to operatos and numbers 
 def split_expression(exspr_string):
-    float_re=r'([0-9]*(\.?)[0-9])'
-    print(float_re)
-    for operator in operation:
-      operator_decoration="(\\"+operator+")"
-      operation_patern=r"^"+float_re+operator_decoration+float_re+"$"
-      print(operation_patern)
-      while re.search(operation_patern,exspr_string):
-        result=(re.sub(operation_patern,calculation(exspr_string,operator),exspr_string))
+  float_re='[0-9]*(\.?)[0-9]*'
+  for operator in operation:
+    oprerator_esc=operator
+    if operator=="*" or operator=="+":
+      oprerator_esc='\\'+operator
+    operation_patern=float_re+oprerator_esc+float_re
+    has_match=re.search(operation_patern,exspr_string)
+    while has_match:
+      exspr_string=(re.sub(operation_patern,calculation(has_match.group(),operator),exspr_string,1))
+      has_match=re.search(operation_patern,exspr_string)
       
-    return result
+  return exspr_string
 
 
 
 
 #start program
-result="2+3+3+3"
-try:
-  print(float(result))
+initial_expresion=result=user_input()
+while True :
+  try:
+    finish_calculation=float(result)
+    print(f"{initial_expresion}={finish_calculation}")
+    break 
 
-except ValueError:
-  while True :
-    result=split_expression(result)
+  except ValueError:
+    if '(' in result and ')' in result:
+      result=split_brackets(result)
+    else:
+      result=split_expression(result)
   
